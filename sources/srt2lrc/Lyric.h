164837207e93496b5f18422dd8f18c8d088f1cc3
mingpingzhang <mingpingzhang@163.com>
2026-04-05 20:43:05 +0800

    new

--- sources/srt2lrc/Lyric.h
+++ sources/srt2lrc/Lyric.h
@@ -7,11 +7,22 @@
 #ifndef LYRIC_H
 #define LYRIC_H
 
+#include <chrono>
+#include <string>
 
 class Lyric
 {
 public:
 	Lyric();
 	~Lyric();
+private:
+	std::chrono::system_clock time_;
+	std::string content_;
+public:
+	const std::chrono::system_clock time()
+	{ return time_;}
+	void setTime(const std::chrono::system_clock& time)
+	{ time_ = time;}
+
 };
 #endif//LYRIC_H
