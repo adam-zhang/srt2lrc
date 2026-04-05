6763e7a0e12d7265cc5a8dcb5cc13e45540e82e6
mingpingzhang <mingpingzhang@163.com>
2026-04-05 19:51:14 +0800

    new

---
+++ sources/srt2lrc/Lyric.cpp
@@ -0,0 +1,16 @@
+/********************************************************************************
+> FileName:	Lyric.cpp
+> Author:	Mingping Zhang
+> Email:	mingpingzhang@163.com
+> Create Time:	Sun Apr  5 19:50:44 2026
+********************************************************************************/
+
+#include "Lyric.h"
+
+Lyric::Lyric()
+{
+}
+
+Lyric::~Lyric()
+{
+}
---
+++ sources/srt2lrc/Lyric.h
@@ -0,0 +1,17 @@
+/********************************************************************************
+> FileName:	Lyric.h
+> Author:	Mingping Zhang
+> Email:	mingpingzhang@163.com
+> Create Time:	Sun Apr  5 19:50:44 2026
+********************************************************************************/
+#ifndef LYRIC_H
+#define LYRIC_H
+
+
+class Lyric
+{
+public:
+	Lyric();
+	~Lyric();
+};
+#endif//LYRIC_H
